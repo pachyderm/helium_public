66bfed5b870fe337045c772f110184ecd96ee55f
Sean Slattery <sean.slattery203@gmail.com>
2022-09-23 19:16:07 -0500

    wip

--- pulumi_backends/gcp_namespace_only/gcp_namespace_only.go
+++ pulumi_backends/gcp_namespace_only/gcp_namespace_only.go
@@ -184,6 +184,7 @@ func CreatePulumiProgram(id, expiry, helmChartVersion, consoleVersion, pachdVers
 			"oauthClientSecret":    pulumi.String("***REMOVED***"),
 			"rootToken":            pulumi.String("***REMOVED***"),
 			"enterpriseSecret":     pulumi.String("***REMOVED***"),
+			"localhostIssuer":      pulumi.String("true"),
 			"enterpriseLicenseKey": pulumi.String("***REMOVED***"), //Set in .circleci/config.yml
 			"storage": pulumi.Map{
 				"google": pulumi.Map{
@@ -211,6 +212,7 @@ func CreatePulumiProgram(id, expiry, helmChartVersion, consoleVersion, pachdVers
 				"oauthClientSecret":    pulumi.String("***REMOVED***"),
 				"rootToken":            pulumi.String("***REMOVED***"),
 				"enterpriseSecret":     pulumi.String("***REMOVED***"),
+				"localhostIssuer":      pulumi.String("true"),
 				"enterpriseLicenseKey": pulumi.String("***REMOVED***"), //Set in .circleci/config.yml
 				"storage": pulumi.Map{
 					"google": pulumi.Map{
@@ -228,18 +230,19 @@ func CreatePulumiProgram(id, expiry, helmChartVersion, consoleVersion, pachdVers
 		}
 		array := []pulumi.AssetOrArchiveInput{}
 		array = append(array, pulumi.AssetOrArchiveInput(pulumi.NewFileAsset(valuesYaml)))
-		corePach, err := helm.NewRelease(ctx, "pach-release", &helm.ReleaseArgs{
-			Atomic:        pulumi.Bool(cleanup2),
-			CleanupOnFail: pulumi.Bool(cleanup2),
-			Timeout:       pulumi.Int(900),
-			Namespace:     namespace.Metadata.Elem().Name(),
-			RepositoryOpts: helm.RepositoryOptsArgs{
-				Repo: pulumi.String("https://helm.***REMOVED***"), //TODO Use Chart files in Repo
+		corePach, err := helm.NewChart(ctx, id, helm.ChartArgs{
+			//	Atomic:        pulumi.Bool(cleanup2),
+			//	CleanupOnFail: pulumi.Bool(cleanup2),
+			//	Timeout:       pulumi.Int(900),
+			Namespace: pulumi.String(id),
+			FetchArgs: helm.FetchArgs{
+				Repo: pulumi.String("https://helm.***REMOVED***"),
 			},
-			Version:        pulumi.String(helmChartVersion),
-			Chart:          pulumi.String("pachyderm"),
-			ValueYamlFiles: pulumi.AssetOrArchiveArray(array), // pulumi.NewFileAsset("./metrics.yml"),
-			//
+			//	RepositoryOpts: helm.RepositoryOptsArgs{
+			//		Repo: pulumi.String("https://helm.***REMOVED***"), //TODO Use Chart files in Repo
+			//	},
+			Version: pulumi.String(helmChartVersion),
+			Chart:   pulumi.String("pachyderm"),
 			Values: pulumi.Map{
 				"deployTarget": pulumi.String("GOOGLE"),
 				"global": pulumi.Map{
@@ -278,14 +281,19 @@ func CreatePulumiProgram(id, expiry, helmChartVersion, consoleVersion, pachdVers
 			return err
 		}
 
-		gcpL4LoadBalancerIP := pulumi.All(corePach.Status.Namespace()).ApplyT(func(args []interface{}) (pulumi.StringOutput, error) {
-			namespace := args[0].(*string)
-			svc, err := corev1.GetService(ctx, "svc", pulumi.ID(fmt.Sprintf("%s/pachyderm-proxy", *namespace)), nil, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "10m"}), pulumi.Provider(k8sProvider))
-			if err != nil {
-				log.Errorf("error getting loadbalancer IP: %v", err)
-			}
+		gcpL4LoadBalancerIP := corePach.GetResource("v1/Service", "pachyderm-proxy", id).ApplyT(func(r interface{}) (pulumi.StringOutput, error) {
+			svc := r.(*corev1.Service)
 			return svc.Status.LoadBalancer().Ingress().Index(pulumi.Int(0)).Ip().Elem(), nil
 		}).(pulumi.StringOutput)
+
+		//gcpL4LoadBalancerIP := pulumi.All(corePach.Status.Namespace()).ApplyT(func(args []interface{}) (pulumi.StringOutput, error) {
+		//	namespace := args[0].(*string)
+		//	svc, err := corev1.GetService(ctx, "svc", pulumi.ID(fmt.Sprintf("%s/pachyderm-proxy", *namespace)), nil, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "10m"}), pulumi.Provider(k8sProvider))
+		//	if err != nil {
+		//		log.Errorf("error getting loadbalancer IP: %v", err)
+		//	}
+		//	return svc.Status.LoadBalancer().Ingress().Index(pulumi.Int(0)).Ip().Elem(), nil
+		//}).(pulumi.StringOutput)
 
 		_, err = dns.NewRecordSet(ctx, "frontendRecordSet", &dns.RecordSetArgs{
 			Name: consoleUrl + ".",
